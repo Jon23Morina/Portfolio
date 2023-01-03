d6250411db9584749cf2debe8d58cd9ad57a0db6
Jon23Morina <jonmorina23@gmail.com>
2023-01-03 15:10:57 +0100

    added typewriter function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Typewriter('#typewriter', {
+    strings: [
+    '"Its not a bug; its an undocumented feature."',
+    '"A good code is a clean code. After you solve the problem."',
+    '"First, solve the problem. Then, write the code."',
+    '"Code is like humor. When you have to explain it, its bad."' 
+    ],
+    autoStart: true,
+    loop: true
+  });
